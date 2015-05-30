3b414236dc9510b9e1cc192938b36fc90101bc1f
Heungsub Lee <sub@subl.ee>
2015-05-31 07:42:19 +0900

    Start to implement a sampling profiler

--- profiling/sampling.py
+++ profiling/sampling.py
@@ -16,7 +16,7 @@ __all__ = ['SamplingProfiler']
 
 class SignalThread(threading.Thread):
 
-    def __init__(self, signum=signal.SIGALRM, interval=0.1):
+    def __init__(self, signum=signal.SIGALRM, interval=0.01):
         threading.Thread.__init__(self)
         self.signum = signum
         self.interval = interval
@@ -54,3 +54,26 @@ class SamplingProfiler(Profiler):
     def stop(self):
         signal.signal(signal.SIGALRM, self.prev_handler)
         self.signal_thread.stop()
+
+    def _profile(self, frame, event, arg):
+        """The callback function to register by :func:`sys.setprofile`."""
+        if event.startswith('c_'):
+            return
+        frame_stack = self._frame_stack(frame)
+        frame_stack.pop()
+        if not frame_stack:
+            return
+        parent_stat = self.stats
+        for f in frame_stack:
+            parent_stat = parent_stat.ensure_child(f.f_code)
+        code = frame.f_code
+        frame_key = id(frame)
+        # if c:
+        #     event = event[2:]
+        #     code = mock_code(arg.__name__)
+        #     frame_key = id(arg)
+        # record
+        if event in ('call',):
+            self._entered(0, code, frame_key, parent_stat)
+        elif event in ('return', 'exception'):
+            self._left(1, code, frame_key, parent_stat)
